7f5b180feee92ced3dba19a6b2605efbb65da51a
Junior <wojunior90@outlook.com>
2021-03-05 22:32:34 -0300

    Implementei as celulas da tela de account e criri a camada de model do account.

---
+++ iPayBF/iPayBF/View/Account/CustomCells/ProductCell.swift
@@ -0,0 +1,27 @@
+//
+//  ProductCell.swift
+//  iPayBF
+//
+//  Created by Wagner Ongaro Junior on 05/03/21.
+//
+
+import UIKit
+
+class ProductCell: UITableViewCell {
+    
+    @IBOutlet weak var productLabel: UILabel!
+    @IBOutlet weak var qtdLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var productImageView: UIImageView!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        
+        
+    }
+
+
+    
+}
