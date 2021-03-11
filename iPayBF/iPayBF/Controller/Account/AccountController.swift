d8eee8b7c44ef1ae4fdf4b3d7ea49d1ee5cf3c49
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2021-03-11 19:35:59 -0300

    Iniciando processo da camada Network

--- iPayBF/iPayBF/Controller/Account/AccountController.swift
+++ iPayBF/iPayBF/Controller/Account/AccountController.swift
@@ -41,63 +41,12 @@ class AccountController {
 	}
 	
 	func loadAccount(completion: @escaping(_ success: Bool, _ error: NSError?) -> Void) {
-		// URL Base String
-		let urlString: String = "https://run.mocky.io/v3/82375610-cc6d-4261-9581-7ec9f0ee1fbe"
 		
-		// URL Base URL
-		guard let url: URL = URL(string: urlString) else { return }
 		
-		// URL Request
-		var urlRequest: URLRequest = URLRequest(url: url)
 		
-		// Configuração do URL Request
-		urlRequest.httpMethod = "GET"
-		urlRequest.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
-		
-		// Chamada de Request
-		AF.request(urlRequest).responseJSON { (response) in
-			
-			// Validação de Status
-			if response.response?.statusCode == 200 {
-				guard let data = response.data else { return }
-				
-				do {
-					
-					let account: Account = try Account(data: data)
-					self.addAccount(account: account)
-					completion(true, nil)
-				} catch {
-					print("Erro no Accout")
-					completion(false, error as NSError)
-				}
-				
-			} else {
-				completion(false, NSError())
-			}
-			
-		}
-	
 	}
 	
-	func loadAccountMock(completion: @escaping(_ account: Account) -> Void) {
-		
-		if let path = Bundle.main.path(forResource: "Account", ofType: "json") {
-			
-			do {
-				
-				let data = try Data(contentsOf: URL(fileURLWithPath: path),
-										  options: .mappedIfSafe)
-				
-				let account: Account = try Account(data: data)
-				
-				completion(account)
-				
-			} catch {
-				print(error)
-			}
-			
-		}
-
+	func loadAccountMock(completion: @escaping(_ success: Bool, _ error: NSError?) -> Void) {
 	}
 	
 }
